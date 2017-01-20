856504833eac9e9775b52ac0f67762a778f5333f
Domenico Gemoli <domenicogemoli@gmail.com>
2017-01-20 22:42:57 +0100

    Avoid using local settings folder

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const exec = require('child_process').exec;
 
-// local directory where selectable setting files are stored
-const LOCAL_SETTINGS_DIRECTORY = 'src/settings';
-// file where current rocket league settings are stored
-const ROCKET_LEAGUE_SETTINGS_FILE = process.env[
+const GAMES_DIRECTORY = process.env[
 	(process.platform == 'win32')
 		? 'USERPROFILE' 
 		: 'HOME'
-] + '\\Documents\\My Games\\Rocket League\\TAGame\\Config\\TASystemSettings.ini';
+] + '\\Documents\\My Games\\';
+// local directory where selectable setting files are stored
+const LOCAL_SETTINGS_DIRECTORY = GAMES_DIRECTORY + 'Rocket League Launcher';
+// file where current rocket league settings are stored
+const ROCKET_LEAGUE_SETTINGS_FILE = GAMES_DIRECTORY + 'Rocket League\\TAGame\\Config\\TASystemSettings.ini';
 
 const MENU = {
 	'-- Launch game': launchGame,
@@ -22,6 +23,10 @@ const MENU = {
 			name: 'name',
 			type: 'string'
 		}]).then((backup) => {
+			if (!fs.existsSync(LOCAL_SETTINGS_DIRECTORY)){
+				fs.mkdirSync(LOCAL_SETTINGS_DIRECTORY);
+			}
+			
 			copyFile(ROCKET_LEAGUE_SETTINGS_FILE, `${LOCAL_SETTINGS_DIRECTORY}/${backup.name}.ini`)
 				.then(showMenu());
 		});
@@ -32,8 +37,6 @@ const MENU = {
 }
 
 function copyFile (source, destination) {
-	console.log(source, destination)
-
 	return new Promise((resolve, reject) => {
 		let strm = fs
 			.createReadStream(source)
